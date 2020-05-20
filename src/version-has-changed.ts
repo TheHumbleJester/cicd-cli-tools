3e915db96fbe5b37ae02b7fa5642652bbf82dc78
The Humble Jester <the.humble.jester@gmail.com>
2020-05-20 08:42:18 +0200

    feat: Add optional argument to define in what file looking for version number

--- src/version-has-changed.ts
+++ src/version-has-changed.ts
@@ -7,11 +7,12 @@ import { exec } from 'child_process';
 const execAsync = promisify(exec);
 
 export async function versionHasChanged() {
+  const file = process.argv[2] || 'package.json';
   /* eslint-disable-next-line import/no-dynamic-require, global-require */
-  const pkg = require(path.resolve('./package.json'));
+  const pkg = require(path.resolve(`./${file}`));
   const currentVersion = pkg.version;
   try {
-    const prevPkg = await execAsync('git show HEAD~1:package.json');
+    const prevPkg = await execAsync(`git show HEAD~1:${file}`);
     const previousVersion = JSON.parse(prevPkg.stdout).version;
 
     if (currentVersion !== previousVersion) {
